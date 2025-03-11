1a61c19ea4b69cab8ba022c1cfb962d6b4b38034
Chris <104437487+Yeesbae@users.noreply.github.com>
2025-03-11 14:33:39 +0800

    Update import files

--- App/urls.py
+++ App/urls.py
@@ -1,6 +1,5 @@
 # Description: This file contains the URL patterns for the app.
 
-# from django.urls import path
 from django.urls import include, path
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
